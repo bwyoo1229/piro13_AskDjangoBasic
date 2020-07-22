e7e0041c28897270627fc5cf6c6c8eacc4f09a89
bwyoo1229 <bwyoo1229@gmail.com>
2020-07-23 01:44:17 +0900

    05 URLConf와 정규 표현식

--- askcompany/urls.py
+++ askcompany/urls.py
@@ -14,15 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.http import HttpResponse
+from django.urls import path, include
 
-
-def mysum(request,x ,y):
-    result = x + y
-    return HttpResponse('result = {}'.format(result))
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('mysum/<int:x>/<int:y>/', mysum),
+    path('shop/', include('shop.urls'))
 ]
