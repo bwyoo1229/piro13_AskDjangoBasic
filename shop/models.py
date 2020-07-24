db0fbc07c57f9d26bcf31a789c7d7b63b1cc41c5
bwyoo1229 <bwyoo1229@gmail.com>
2020-07-25 00:21:57 +0900

    14 모델을 통한 데이터 생성/수정/삭제

--- shop/models.py
+++ shop/models.py
@@ -33,15 +33,18 @@ class Tag(models.Model):
 
 
 class Item(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, validators=[])
     desc = models.TextField(blank=True)
     price = models.PositiveIntegerField()
     is_publish = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return f'<{self.pk}> {self.name}'
+
     class Meta:
-        ordering = ['-id']
+        ordering = ['id']
 
 
 
