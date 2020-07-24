0de4c3fa7c1577de60662ca70fac9b29ee73014d
bwyoo1229 <bwyoo1229@gmail.com>
2020-07-24 22:56:11 +0900

    13 모델을 통한 데이터 조회

--- shop/models.py
+++ shop/models.py
@@ -40,8 +40,10 @@ class Item(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    def __str__(self):
-        return f'<{self.pk}> {self.name}'
+    class Meta:
+        ordering = ['-id']
+
+
 
 
 
